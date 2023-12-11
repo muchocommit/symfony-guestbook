264684728a96613287da6fc61490967fc7ef6331
Vadim Korabelnik <vadimkorabelnik@yandex.ru>
2023-12-11 16:43:46 +0300

    Added cache for header and mainpage, added sample output cache

---
+++ guestbook/src/Command/StepInfoCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Contracts\Cache\CacheInterface;
+
+#[AsCommand('app:step:info')]
+class StepInfoCommand extends Command
+{
+    public function __construct(
+        private CacheInterface $cache,
+    ) {
+        parent::__construct();
+    }
+
+    /*
+     * Sample caching of command output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $step = $this->cache->get('app.current_step', function ($item) {
+            $process = new Process(['git', 'reflog']);
+            $process->mustRun();
+            $item->expiresAfter(30);
+
+            return $process->getOutput();
+        });
+        $output->writeln($step);
+
+        return Command::SUCCESS;
+    }
+}
